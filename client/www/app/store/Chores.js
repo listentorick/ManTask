d955f5a2188bf5c75235dfa30a3ecb00c0b843e5
unknown <rick@RICK-LT.(none)>
2012-07-07 10:06:38 +0100

    Added grouping to chores

--- client/www/app/store/Chores.js
+++ client/www/app/store/Chores.js
@@ -7,6 +7,18 @@ Ext.define("ManTask.store.Chores", {
             type: 'localstorage',
             id: 'chores-app-store'
         },
-        sorters: [{ property: 'dateCreated', direction: 'DESC'}]
+        sorters: [{ property: 'dateCreated', direction: 'DESC'}],
+		grouper: {
+            sortProperty: "dateCreated",
+            direction: "DESC",
+            groupFn: function (record) {
+
+                if (record && record.data.dateCreated) {
+                    return record.data.dateCreated.toDateString();
+                } else {
+                    return '';
+                }
+            }
+        }
     }
 });
--- client/www/app/view/ChoresList.js
+++ client/www/app/view/ChoresList.js
@@ -2,9 +2,12 @@ Ext.define("ManTask.view.ChoresList", {
     extend: "Ext.dataview.List",
     alias: "widget.choreslist",
     config: {
+		scrollable:'vertical',
         loadingText: "Loading Chores...",
         emptyText: '</pre><div class="notes-list-empty-text">No notes found.</div><pre>',
         onItemDisclosure: true,
         itemTpl: '</pre><div class="list-item-title">{title}</div><div class="list-item-narrative">{narrative}</div><pre>',
+		grouped: true
     }
 });
+
